34d266dcd2fd4d7f9224eaa86546e54f7ed4e11a
John <johnmave126@gmail.com>
2018-10-02 23:57:22 -0400

    Use array style of calling command

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { delimiter, resolve as pathResolve } from 'path'
 import promiseTimeout from 'timeout-as-promise'
 import spawn from 'cross-spawn'
-import spawnArgs from 'spawn-args'
 
 const timedOutSymbol = Symbol('TIMED_OUT')
 
@@ -20,38 +19,32 @@ export default function run (command = [], options = {}) {
 
   // Returning an async function lets gulp know it's complete when the promise resolves
   return async () => {
-    // Loop over each command and run them one-at-a-time
-    for (let cmd of commands) {
-      // Wrap it in a try/catch to ignore errors if ignoreErrors is set
-      try {
-        // Run the command and add the promise to the promise array
-        const promiseArray = [runCommand(cmd, quiet,ignoreErrors, pathResolve(cwd), env)]
-        // If timeout is set, then add that as a timebomb to the promise array
-        if (typeof timeout !== 'undefined') promiseArray.push(promiseTimeout(parseInt(timeout, 10), timedOutSymbol))
+    // Wrap it in a try/catch to ignore errors if ignoreErrors is set
+    try {
+      // Run the command and add the promise to the promise array
+      const promiseArray = [runCommand(cmd, quiet,ignoreErrors, pathResolve(cwd), env)]
+      // If timeout is set, then add that as a timebomb to the promise array
+      if (typeof timeout !== 'undefined') promiseArray.push(promiseTimeout(parseInt(timeout, 10), timedOutSymbol))
 
-        // Finally, await on a race between the promises to deal with the first of them
-        const promiseResult = await Promise.race(promiseArray)
+      // Finally, await on a race between the promises to deal with the first of them
+      const promiseResult = await Promise.race(promiseArray)
 
-        // If the promise resolves with timedOutSymbol first, then throw an error
-        if (promiseResult === timedOutSymbol) {
-          throw new Error('Process timed out...')
-        }
-      } catch (err) {
-        // Unless ignoreErrors is true, re-throw the caught error
-        if (!ignoreErrors) {
-          throw err
-        }
+      // If the promise resolves with timedOutSymbol first, then throw an error
+      if (promiseResult === timedOutSymbol) {
+        throw new Error('Process timed out...')
+      }
+    } catch (err) {
+      // Unless ignoreErrors is true, re-throw the caught error
+      if (!ignoreErrors) {
+        throw err
       }
     }
   }
 }
 
 const runCommand = (command, quiet,ignoreErrors, cwd, env) => new Promise((resolve) => {
-  // Parse out the args from the command
-  const args = spawnArgs(command, { removequotes: 'always' })
-
   // Run the command
-  const proc = spawn(args.shift(), args, {
+  const proc = spawn(command.shift(), command, {
     stdio: [
       'ignore', // ignore stdin
       (quiet ? 'ignore' : 'inherit'), // ignore or inherit stdout depending on quiet flag
